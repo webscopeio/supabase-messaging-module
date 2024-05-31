76c354d39427b2fa1790982cef15a9c5daf34ae8
Jan Koriťák <koritakjan@gmail.com>
2024-05-31 21:46:15 +0200

    feat: createRoom and inviteToRoom functuon for useRooms

--- modules/messaging/hooks/useRooms.ts
+++ modules/messaging/hooks/useRooms.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 import { createClient } from "@/modules/utils/client"
 
@@ -6,20 +6,40 @@ import { Room } from ".."
 
 type UseRoomsProps = {
   rooms: Room[]
+  onCreate?: VoidFunction
 }
+
+type CreateRoom = ({
+  name,
+  userIds,
+}: {
+  name: string
+  userIds: Array<string>
+}) => void
+
+type InviteToRoom = ({
+  roomId,
+  userIds,
+}: {
+  roomId: number
+  userIds: Array<string>
+}) => void
 
 type UseRoomsReturnType = {
   rooms: Room[]
+  createRoom: CreateRoom
+  inviteToRoom: InviteToRoom
 }
+
+const client = createClient()
 
 export const useRooms = ({
   rooms: roomsInitial,
+  onCreate,
 }: UseRoomsProps): UseRoomsReturnType => {
   const [rooms, setRooms] = useState(roomsInitial)
 
   useEffect(() => {
-    const client = createClient()
-
     const changes = client
       .channel("schema-db-changes")
       .on<Room>(
@@ -56,7 +76,46 @@ export const useRooms = ({
     return () => void changes.unsubscribe()
   }, [rooms, setRooms])
 
+  const createRoom = useCallback<CreateRoom>(
+    async ({ name, userIds }) => {
+      try {
+        const room = await client
+          .from("rooms")
+          .insert({
+            name,
+          })
+          .select()
+
+        // TODO: Fix the -1
+        const roomId = room.data?.at(0)?.id ?? -1
+
+        await client.from("room_participants").insert(
+          userIds.map((user_id) => ({
+            user_id,
+            room_id: roomId,
+          }))
+        )
+
+        onCreate?.()
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    [onCreate]
+  )
+
+  const inviteToRoom = useCallback<InviteToRoom>(({ roomId, userIds }) => {
+    void client.from("room_participants").insert(
+      userIds.map((user_id) => ({
+        user_id,
+        room_id: roomId,
+      }))
+    )
+  }, [])
+
   return {
     rooms,
+    createRoom,
+    inviteToRoom,
   }
 }
--- modules/messaging/index.ts
+++ modules/messaging/index.ts
@@ -4,22 +4,6 @@ import { createClient } from "@/modules/utils/server"
 export type Room = Database["public"]["Tables"]["rooms"]["Row"]
 export type Message = Database["public"]["Tables"]["messages"]["Row"]
 
-type ServerError = {
-  error: { message: string }
-}
-
-export async function createRoom({
-  name,
-}: {
-  name: string
-}): Promise<ServerError | void> {
-  const supabase = createClient()
-  const { error } = await supabase.from("rooms").insert({
-    name,
-  })
-  if (error) return { error: { message: error.message } }
-}
-
 export async function getRooms(): Promise<Room[]> {
   const supabase = createClient()
   const { data, error } = await supabase.from("rooms").select("*")
