254fc8e12b9cca2a8d98198afc39c1eb5b71a20c
Jan Koriťák <koritakjan@gmail.com>
2024-05-31 19:12:41 +0200

    revert: test

--- app/messaging/layout.tsx
+++ app/messaging/layout.tsx
@@ -3,7 +3,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <section className="mx-auto max-w-5xl space-y-6 py-6">
       <header className="space-y-2">
         <h2 className="text-4xl font-semibold tracking-tight lg:text-5xl">
-          Messagingasd
+          Messaging
         </h2>
       </header>
       <div className="w-full">{children}</div>
