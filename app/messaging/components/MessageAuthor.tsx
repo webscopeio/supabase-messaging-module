cc30c47c46d770d0c409f9a0f81a6df2da1a221a
martinbuchta <martin.buchta@solarwinds.com>
2024-06-01 10:33:19 +0200

    feat: add tanstack query for caching users

--- app/messaging/components/MessageAuthor.tsx
+++ app/messaging/components/MessageAuthor.tsx
@@ -1,25 +1,26 @@
-import { useState } from "react"
+"use client"
+
+import { useQuery } from "@tanstack/react-query"
 
 import { Skeleton } from "@/components/ui/skeleton"
 import { AvatarPlaceholder } from "@/components/avatar-placeholder"
 
-import { User } from "@/modules/messaging"
 import useUsers from "@/modules/messaging/hooks/useUsers"
 
 export default function MessageAuthor({ user_id }: { user_id: string }) {
-  const [user, setUser] = useState<User>()
-
   const { getUserById } = useUsers()
-  getUserById({ id: user_id }).then((user) => {
-    setUser(user)
+
+  const query = useQuery({
+    queryKey: ["user", user_id],
+    queryFn: () => getUserById({ id: user_id }),
   })
 
   return (
     <div className="mb-2 flex items-center gap-2">
-      {user ? (
+      {query.data ? (
         <>
-          <AvatarPlaceholder preferredHue={user?.preferred_hue || "123"} />
-          <h3 className="font-bold">{user?.email || user_id}</h3>
+          <AvatarPlaceholder preferredHue={query.data.preferred_hue || "123"} />
+          <h3 className="font-bold">{query.data.email || user_id}</h3>
         </>
       ) : (
         <>
